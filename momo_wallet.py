e0ca603f28f9c5dfd76e0488a4f368213366c79c
Nguyễn Ngô Quốc Thịnh <nguyenngothinh669@gmail.com>
2026-06-15 15:28:20 +0700

    update commit

--- momo_wallet.py
+++ momo_wallet.py
@@ -5,6 +5,8 @@ logging.basicConfig(
     filename="momo_transactions.log",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt='%d-%m-%Y %H:%M:%S' ,
+    encoding="utf-8" 
 )
 
 
