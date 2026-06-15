6d386c00124356584b1326a6ccaf02c293b13c34
Nguyễn Ngô Quốc Thịnh <nguyenngothinh669@gmail.com>
2026-06-15 12:49:30 +0700

    update file

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,3 @@
-"""Module định nghĩa các ngoại lệ tùy chỉnh cho hệ thống Ví MoMo."""
-
-
 class InvalidAmountError(Exception):
     """Ngoại lệ xảy ra khi số tiền giao dịch nhỏ hơn hoặc bằng 0."""
 
